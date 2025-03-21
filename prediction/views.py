32cd94367bbaf7050a019afa3ff4b1a2838382b8
kunotakato <sprinter.0729@gmail.com>
2025-03-21 10:47:02 +0900

    second commit
    画像判定できないので、修正必要

--- prediction/views.py
+++ prediction/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.models import save_model  
-
-# VGG16モデルのロード
-model = VGG16(weights='imagenet')
-save_model(model, 'vgg16.h5')  
-
 import numpy as np
 from .forms import ImageUploadForm
 from .models import UploadedImage
 
-
+# VGG16モデルのロード
+model = VGG16(weights='imagenet')
+save_model(model, 'vgg16.h5')  
 
 def predict_image(request):
     if request.method == 'POST':
@@ -20,18 +18,21 @@ def predict_image(request):
             img_path = uploaded_image.image.path
 
             # 画像の前処理
-            img = image.load_img(img_path, target_size=(224, 224))
-            img_array = image.img_to_array(img)
+             #画像のリサイズ
+            img = load_img(img_path, target_size=(224, 224))
+             # 画像をNumpy配列に変換
+            img_array = img_to_array(img)
             img_array = np.expand_dims(img_array, axis=0)
             img_array = preprocess_input(img_array)
 
             # 画像の判定
             predictions = model.predict(img_array)
+            # 予測結果を解釈し、表示用の結果を生成
             results = decode_predictions(predictions, top=5)[0]
 
-            return render(request, 'prediction/result.html', {'results': results, 'image_url': uploaded_image.image.url})
+            return render(request, 'home.html', {'results': results, 'image_url': uploaded_image.image.url})
 
     else:
         form = ImageUploadForm()
 
-    return render(request, 'prediction/upload.html', {'form': form})
+    return render(request, 'home.html', {'form': form})
